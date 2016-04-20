f3e317272d304df8660938133f1fd50036c8f586
Ted Zhu <tedz2usa@gmail.com>
2016-04-20 09:03:52 -0700

    Add new table styling, and convert to HTML5 document.

--- index.php
+++ index.php
@@ -21,32 +21,62 @@
 		array_push($devices, $row);
 	}
 
-	o($devices);
+	//o($devices);
 
 ?>
-<br>
-<h1>TAN Messaging App</h1>
-<h2>Available Devices</h2>
-<table>
-	<tr>
-		<th>Device ID</th>
-		<th>Name</th>
-		<th>MAC Address</th>
-	</tr>
-	<?php
+<!DOCTYPE html> 
+<html>
+	<head>
+		<title>TAN Messaging App</title>
 
-	foreach ($devices as $device) {
-		?>
-		<tr>
-			<td><?php echo $device['id']; ?></td>
-			<td><?php echo $device['name']; ?></td>
-			<td><?php echo $device['mac_address']; ?></td>
-		</tr>
-		<?php
-	}
+<style>
 
-	?>
-</table>
+table {
+	border-collapse: collapse;
+}
+
+table, tr, td, th {
+	border: 1px solid #000;
+}
+
+th, td {
+	text-align: left;
+	padding: 5px;
+	padding-right: 15px;
+}
+
+
+
+</style>
+
+	</head>
+	<body>
+		<h1>TAN Messaging App</h1>
+		<h2>Devices</h2>
+		<table>
+			<tr>
+				<th>Device ID</th>
+				<th>Name</th>
+				<th>MAC Address</th>
+			</tr>
+			<?php
+
+			foreach ($devices as $device) {
+				?>
+				<tr>
+					<td><?php echo $device['id']; ?></td>
+					<td><?php echo $device['name']; ?></td>
+					<td><?php echo $device['mac_address']; ?></td>
+				</tr>
+				<?php
+			}
+
+			?>
+		</table>
+	</body>
+
+
+</html>
 
 <?php
 
